bbba2f3d048ede0ac595ae8bcb4b08c133183fe3
prettyDamCool <48737752+prettyDamCool@users.noreply.github.com>
2019-03-19 23:09:04 +0000

    Add files via upload
    crappy project i found i made in 2017

---
+++ encryption.py
@@ -0,0 +1,66 @@
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+import os, random, sys, pkg_resources
+ 
+def encrypt(key, filename):
+        chunksize = 64 * 1024
+        outFile = os.path.join(os.path.dirname(filename), "(encrypted)"+os.path.basename(filename))
+        filesize = str(os.path.getsize(filename)).zfill(16)
+        IV = ''
+ 
+        for i in range(16):
+                IV += chr(random.randint(0, 0xFF))
+       
+        encryptor = AES.new(key, AES.MODE_CBC, IV)
+ 
+        with open(filename, "rb") as infile:
+                with open(outFile, "wb") as outfile:
+                        outfile.write(filesize)
+                        outfile.write(IV)
+                        while True:
+                                chunk = infile.read(chunksize)
+                               
+                                if len(chunk) == 0:
+                                        break
+ 
+                                elif len(chunk) % 16 !=0:
+                                        chunk += ' ' *  (16 - (len(chunk) % 16))
+ 
+                                outfile.write(encryptor.encrypt(chunk))
+ 
+ 
+
+       
+def allfiles():
+        allFiles = []
+        for root, subfiles, files in os.walk(os.getcwd()):
+                for names in files:
+                        allFiles.append(os.path.join(root, names))
+ 
+        return allFiles
+ 
+       
+choice = raw_input("Do you want to (E)ncrypt")
+password = raw_input("Enter the password: ")
+ 
+encFiles = allfiles()
+ 
+if choice == "E":
+        for Tfiles in encFiles:
+                if os.path.basename(Tfiles).startswith("(encrypted)"):
+                        print "%s is already encrypted" %str(Tfiles)
+                        pass
+ 
+                elif Tfiles == os.path.join(os.getcwd(), sys.argv[0]):
+                        pass
+                else:
+                        encrypt(SHA256.new(password).digest(), str(Tfiles))
+                        print "Done encrypting %s" %str(Tfiles)
+                        os.remove(Tfiles)
+ 
+ 
+
+ 
+else:
+        print "Please choose a valid command."
+        sys.exit()
